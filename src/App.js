12099cd7ad499bf410969e53621f08300116f2d2
Teebreezy101 <tammy.batubo@gmail.com>
2020-11-10 16:34:26 +0100

    refactor and add comments

--- src/App.js
+++ src/App.js
@@ -3,170 +3,194 @@ import './App.css';
 import Button from './Button';
 
 function App() {
-  const [display, setDisplay] = useState("0");
+  const [display, setDisplay] = useState('0');
   const [previousValue, setPreviousValue] = useState(null);
   const [operator, setOperator] = useState(null);
+  //times used to store previous state if 2 operators are pressed consecutively
+  const [times, setTimes] = useState(null);
+  // const [showPressed, setShowPressed] = useState('');
 
   const compute = (value) => () => {
-    const num = Number(value);
     const displayNumber = Number(display);
 
     switch (value) {
+      // Cancel functionality
       case 'C':
-        setDisplay("0")
-        setPreviousValue(null)
-        setOperator(null)
+        setDisplay('0');
+        setPreviousValue(null);
+        setOperator(null);
         break;
 
+      // Percentage functionality
       case '%':
-        setDisplay(`${displayNumber / 100}`)
-        setPreviousValue(null)
-        setOperator(null)
-        break
+        setDisplay(`${displayNumber / 100}`);
+        setPreviousValue(null);
+        setOperator(null);
+        break;
 
+      // ± functionality
       case '±':
-        setDisplay(`${displayNumber * -1 }`)
-        break
+        setDisplay(`${displayNumber * -1}`);
+        break;
 
+      // Append . functionality
       case '.':
-        if(display.includes('.')) return;
-        setDisplay(display + '.')
-        break
+        if (display.includes('.')) return;
+        setDisplay(display + '.');
+        break;
 
+      // Addition functionality
       case '+':
-        if(operator !== null) {
-          if(operator === '+') {
-            setPreviousValue(previousValue + displayNumber)
+        if (operator !== null) {
+          //  if plus is pressed after another operator, set operator to plus
+          if (operator.length >= 2) {
+            setOperator('+');
+            setPreviousValue(times + displayNumber);
+            console.log(operator);
           }
-          else if(operator === '-') {
-            setPreviousValue(previousValue - displayNumber)
+          if (operator === '+') {
+            setPreviousValue(previousValue + displayNumber);
+          } else if (operator === '-') {
+            setOperator('+');
+            setPreviousValue(previousValue - displayNumber);
           }
-          else if(operator === '*') {
-            setPreviousValue(previousValue * displayNumber)
-          } 
-          else if(operator === '/') {
-            setPreviousValue(previousValue / displayNumber)
-          }  
-        }else {
-          setPreviousValue(displayNumber)
+          // Allows chaining of operators
+          else if (operator === '*' && display !== '0') {
+            setOperator('+');
+            setPreviousValue(previousValue * displayNumber);
+          }
+          // if times and plus are pressed consecutively set operator to plus. 5 * + 3 = 8
+          else if (operator === '*' && display === '0') {
+            setOperator('+');
+          } else if (operator === '÷') {
+            setPreviousValue(previousValue / displayNumber);
+          }
+        } else {
+          setPreviousValue(displayNumber);
+          setOperator('+');
         }
-        setDisplay('0')
-        setOperator('+')
-        break
+        setDisplay('0');
+        break;
 
+      // Subtract functionality
       case '-':
-        if(operator !== null) {
-          if(operator === '+') {
-            setPreviousValue(previousValue + displayNumber)
+        if (operator !== null) {
+          if (operator === '+') {
+            //if prev operator is plus and minus is pressed set operator to minus. => 5 + - 3 = 2
+            setOperator('-');
+            setPreviousValue(previousValue + displayNumber);
+          } else if (operator === '-') {
+            setPreviousValue(previousValue - displayNumber);
+          } else if (operator === '*') {
+            // Check if display is '0' then 2 operators were pressed consecutively else allow operator chaining. i.e 5 * 3 - 2 ...
+            // if times is pressed followed by minus consecutively, set operator to '* -' which evaluates when equal to is pressed. Also store previousValue in times => 5 * - 5 = -25
+            setOperator(display === '0' ? '* -' : '-');
+            setTimes(previousValue);
+            setPreviousValue(previousValue * displayNumber);
+          } else if (operator === '÷') {
+            setPreviousValue(previousValue / displayNumber);
           }
-          else if(operator === '-') {
-            setPreviousValue(previousValue - displayNumber)
-          }
-          else if(operator === '*') {
-            setPreviousValue(previousValue * displayNumber)
-          } 
-          else if(operator === '/') {
-            setPreviousValue(previousValue / displayNumber)
-          }  
-        }else {
-          setPreviousValue(displayNumber)
+        } else {
+          setPreviousValue(displayNumber);
+          setOperator('-');
         }
-        setDisplay('0')
-        setOperator('-')
-        break
+        setDisplay('0');
+        break;
 
+      // Multiply functionality
       case 'x':
-        if(operator !== null) {
-          if(operator === '+') {
-            setPreviousValue(previousValue + displayNumber)
+        if (operator !== null) {
+          if (operator === '+') {
+            setOperator('*');
+            setPreviousValue(previousValue + displayNumber);
+          } else if (operator === '-') {
+            setOperator('*');
+            setPreviousValue(previousValue - displayNumber);
+          } else if (operator === '*') {
+            setPreviousValue(previousValue * displayNumber);
+          } else if (operator === '÷') {
+            setPreviousValue(previousValue / displayNumber);
           }
-          else if(operator === '-') {
-            setPreviousValue(previousValue - displayNumber)
-          }
-          else if(operator === '*') {
-            setPreviousValue(previousValue * displayNumber)
-          } 
-          else if(operator === '/') {
-            setPreviousValue(previousValue / displayNumber)
-          }  
-        }else {
-          setPreviousValue(displayNumber)
+        } else {
+          setPreviousValue(displayNumber);
+          setOperator('*');
         }
-        setDisplay('0')
-        setOperator('*')
-        break
+        setDisplay('0');
+        break;
 
+      // Divide functionality
       case '÷':
-        if(operator !== null) {
-          if(operator === '+') {
-            setPreviousValue(previousValue + displayNumber)
+        if (operator !== null) {
+          if (operator === '+') {
+            setPreviousValue(previousValue + displayNumber);
+          } else if (operator === '-') {
+            setOperator('÷');
+            setPreviousValue(previousValue - displayNumber);
+          } else if (operator === '*') {
+            setPreviousValue(previousValue * displayNumber);
+          } else if (operator === '÷') {
+            setPreviousValue(previousValue / displayNumber);
           }
-          else if(operator === '-') {
-            setPreviousValue(previousValue - displayNumber)
-          }
-          else if(operator === '*') {
-            setPreviousValue(previousValue * displayNumber)
-          } 
-          else if(operator === '/') {
-            setPreviousValue(previousValue / displayNumber)
-          }  
-        }else {
-          setPreviousValue(displayNumber)
+        } else {
+          setPreviousValue(displayNumber);
+          setOperator('÷');
         }
-        setDisplay('0')
-        setOperator('/')
-        break
+        setDisplay('0');
+        break;
 
-
+      // Equal to functionality
       case '=':
-        if(!operator) return;
-      
-        if(operator === '+') {
-          setDisplay(`${previousValue + displayNumber}`)
-        }else if(operator === '-') {
-          setDisplay(`${previousValue - displayNumber}`)
-        }else if(operator === '*') {
-          setDisplay(`${previousValue * displayNumber}`)
-        }else if(operator === '/') {
-          setDisplay(`${previousValue / displayNumber}`)
+        if (!operator) return;
+
+        if (operator === '+') {
+          setDisplay(`${previousValue + displayNumber}`);
+        } else if (operator === '-') {
+          setDisplay(`${previousValue - displayNumber}`);
+        } else if (operator === '*') {
+          setDisplay(`${previousValue * displayNumber}`);
+        } else if (operator === '÷') {
+          setDisplay(`${previousValue / displayNumber}`);
+        } else if (operator === '* -') {
+          // Use times value which was stored from previousValue to set the display since previousValue will change to 0
+          setDisplay(times * -displayNumber);
         }
-        break
+        setOperator(null);
+        break;
 
-      default: 
-      if(display[display.length - 1] === '.') {
-        setDisplay(display + value)
-      }else {
-        setDisplay(`${Number(display + num)}`)
-      }
+      default:
+        if (display[display.length - 1] === '.') {
+          setDisplay(display + value);
+        } else {
+          setDisplay(`${Number(display + Number(value))}`);
+        }
     }
-    
-  }
-
+  };
 
   return (
     <div className="App">
-      <div className={`display ${display.length >= 8 ? 'reduce' : ''}`}>{`${parseFloat(Number(display).toFixed(4))}`}</div>
+      <div
+        className={`display ${display.length >= 8 ? 'reduce' : ''}`}
+      >{`${parseFloat(Number(display).toFixed(4))}`}</div>
       <div className="buttons">
-        <Button value='C' handleClick={compute} />
-        <Button value='±' type='function' handleClick={compute} />
-        <Button value='÷' type='function' handleClick={compute} />
-        <Button value='x' type='function' handleClick={compute} />
-        <Button value='7' handleClick={compute} />
-        <Button value='8' handleClick={compute} />
-        <Button value='9' handleClick={compute} />
-        <Button value='-' type='function' handleClick={compute} />
-        <Button value='4' handleClick={compute} />
-        <Button value='5' handleClick={compute} />
-        <Button value='6' handleClick={compute} />
-        <Button value='+' type='function' handleClick={compute} />
-        <Button value='1' handleClick={compute} />
-        <Button value='2' handleClick={compute} />
-        <Button value='3' handleClick={compute} />
-        <Button value='=' type='function' handleClick={compute} id = 'equals'/>
-        <Button value='0' handleClick={compute} />
-        <Button value='.' handleClick={compute} />
-        <Button value='%' handleClick={compute} />
-
+        <Button value="C" handleClick={compute} />
+        <Button value="±" type="function" handleClick={compute} />
+        <Button value="÷" type="function" handleClick={compute} />
+        <Button value="x" type="function" handleClick={compute} />
+        <Button value="7" handleClick={compute} />
+        <Button value="8" handleClick={compute} />
+        <Button value="9" handleClick={compute} />
+        <Button value="-" type="function" handleClick={compute} />
+        <Button value="4" handleClick={compute} />
+        <Button value="5" handleClick={compute} />
+        <Button value="6" handleClick={compute} />
+        <Button value="+" type="function" handleClick={compute} />
+        <Button value="1" handleClick={compute} />
+        <Button value="2" handleClick={compute} />
+        <Button value="3" handleClick={compute} />
+        <Button value="=" type="function" handleClick={compute} id="equals" />
+        <Button value="0" handleClick={compute} />
+        <Button value="." handleClick={compute} />
+        <Button value="%" handleClick={compute} />
       </div>
     </div>
   );
